e0e6ac7d5f4c7854cc7e13ca300ad132bc850097
jana <janam9097@gmail.com>
2026-07-24 10:48:25 +0530

    Fix base path to /BT/

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/your-repo-name/", // must exactly match your GitHub repo name
+  base: "/BT/",
 });
