f192b2bd2c564014d9b559ea6bead36d4ff2fe3d
jana <janam9097@gmail.com>
2026-07-24 21:03:40 +0530

    updated  the css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,43 @@
 import logo from "../../assets/images/logo.png";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaUser, FaHeart, FaShoppingCart } from "react-icons/fa";
 import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <nav className="Nav">
-            <img src={logo} alt="Store logo" />
-             <ul>
-                <li>MEN</li>
-                <li>WOMEN</li>
-                <li>KIDS</li>
-                <li>HOME</li>
-             </ul>
-             <div className="search-box">
-             <FaSearch className="search-icon" />
+  return (
+    <nav className="Nav">
+      <img src={logo} alt="Store logo" />
 
-             <input
-              type="text"
-              placeholder="Search"
-              className="search"
-              aria-label="Search products"
-                          />
-            </div>
-            <div className="actions">
-                <button className="button">Login</button>
-                <button className="button">Wishlist</button>
-                <button className="button">Cart</button>
-            </div>
-        </nav>
-    );
+      <ul>
+        <li>MEN</li>
+        <li>WOMEN</li>
+        <li>KIDS</li>
+        <li>HOME</li>
+      </ul>
+
+      <div className="search-box">
+        <FaSearch className="search-icon" />
+
+        <input
+          type="text"
+          placeholder="Search"
+          className="search"
+          aria-label="Search products"
+        />
+      </div>
+
+      <div className="actions">
+        <button className="icon-button" aria-label="Login">
+          <FaUser />
+        </button>
+        <button className="icon-button" aria-label="Wishlist">
+          <FaHeart />
+        </button>
+        <button className="icon-button" aria-label="Cart">
+          <FaShoppingCart />
+        </button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
