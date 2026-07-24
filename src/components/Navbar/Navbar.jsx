32dc30a3d6b353e97102438e251b9ce306f711b0
jana <janam9097@gmail.com>
2026-07-24 21:00:46 +0530

    updated  the css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,34 @@
 import logo from "../../assets/images/logo.png";
-import {
-  FaSearch,
-  FaUser,
-  FaHeart,
-  FaShoppingCart,
-} from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import "./Navbar.css";
 
 function Navbar() {
-  return (
-    <nav className="Nav">
-      <img src={logo} alt="Store logo" />
+    return (
+        <nav className="Nav">
+            <img src={logo} alt="Store logo" />
+             <ul>
+                <li>MEN</li>
+                <li>WOMEN</li>
+                <li>KIDS</li>
+                <li>HOME</li>
+             </ul>
+             <div className="search-box">
+             <FaSearch className="search-icon" />
 
-      <ul>
-        <li>MEN</li>
-        <li>WOMEN</li>
-        <li>KIDS</li>
-        <li>HOME</li>
-      </ul>
-
-      <div className="search-box">
-        <FaSearch className="search-icon" />
-
-        <input
-          type="text"
-          placeholder="Search"
-          className="search"
-          aria-label="Search products"
-        />
-      </div>
-
-      <div className="actions">
-        <button className="icon-button">
-          <FaUser />
-        </button>
-
-        <button className="icon-button">
-          <FaHeart />
-        </button>
-
-        <button className="icon-button">
-          <FaShoppingCart />
-        </button>
-      </div>
-    </nav>
-  );
+             <input
+              type="text"
+              placeholder="Search"
+              className="search"
+              aria-label="Search products"
+                          />
+            </div>
+            <div className="actions">
+                <button className="button">Login</button>
+                <button className="button">Wishlist</button>
+                <button className="button">Cart</button>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
