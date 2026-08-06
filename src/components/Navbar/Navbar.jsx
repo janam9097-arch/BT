c6ec2e8e2aea6a93987993bd4dde18af4d92f89b
jana <janam9097@gmail.com>
2026-08-06 18:00:37 +0530

    Enhance mobile UI: add category links to mobile hamburger menu & luxury bottom nav styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -86,52 +86,95 @@ function Navbar() {
 
           {menuOpen && (
             <div className="hamburger-dropdown-menu">
-              <div className="hamburger-menu-header">
-                <span className="hamburger-menu-title">Account & Support</span>
+              {/* Category Quick Links (Mobile view) */}
+              <div className="hamburger-menu-section mobile-nav-section">
+                <div className="hamburger-menu-header">
+                  <span className="hamburger-menu-title">Explore</span>
+                </div>
+                <ul className="hamburger-menu-list">
+                  <li>
+                    <Link to="/products" className="hamburger-menu-item" onClick={() => setMenuOpen(false)}>
+                      <span>Shop All</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/products?category=men" className="hamburger-menu-item" onClick={() => setMenuOpen(false)}>
+                      <span>Men's Collection</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/products?category=women" className="hamburger-menu-item" onClick={() => setMenuOpen(false)}>
+                      <span>Women's Collection</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/products?category=kids" className="hamburger-menu-item" onClick={() => setMenuOpen(false)}>
+                      <span>Kids Collection</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/categories" className="hamburger-menu-item" onClick={() => setMenuOpen(false)}>
+                      <span>Categories</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/products?filter=sale" className="hamburger-menu-item nav-sale-text" onClick={() => setMenuOpen(false)}>
+                      <span>🔥 Special SALE</span>
+                    </Link>
+                  </li>
+                </ul>
+                <div className="hamburger-menu-divider" />
               </div>
-              <ul className="hamburger-menu-list">
-                <li>
-                  <Link
-                    to="/orders"
-                    className="hamburger-menu-item"
-                    onClick={() => setMenuOpen(false)}
-                  >
-                    <FaBox className="hamburger-item-icon" />
-                    <span>Orders</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/faqs"
-                    className="hamburger-menu-item"
-                    onClick={() => setMenuOpen(false)}
-                  >
-                    <FaHeadset className="hamburger-item-icon" />
-                    <span>Help Center</span>
-                  </Link>
-                </li>
-                <li className="hamburger-menu-divider" />
-                <li>
-                  {user ? (
-                    <button
-                      onClick={handleLogout}
-                      className="hamburger-menu-item logout-item"
-                    >
-                      <FaSignOutAlt className="hamburger-item-icon" />
-                      <span>Log Out</span>
-                    </button>
-                  ) : (
+
+              {/* Account & Support Section */}
+              <div className="hamburger-menu-section">
+                <div className="hamburger-menu-header">
+                  <span className="hamburger-menu-title">Account & Support</span>
+                </div>
+                <ul className="hamburger-menu-list">
+                  <li>
                     <Link
-                      to="/login"
-                      className="hamburger-menu-item login-item"
+                      to="/orders"
+                      className="hamburger-menu-item"
                       onClick={() => setMenuOpen(false)}
                     >
-                      <FaSignInAlt className="hamburger-item-icon" />
-                      <span>Log In</span>
+                      <FaBox className="hamburger-item-icon" />
+                      <span>Orders</span>
                     </Link>
-                  )}
-                </li>
-              </ul>
+                  </li>
+                  <li>
+                    <Link
+                      to="/faqs"
+                      className="hamburger-menu-item"
+                      onClick={() => setMenuOpen(false)}
+                    >
+                      <FaHeadset className="hamburger-item-icon" />
+                      <span>Help Center</span>
+                    </Link>
+                  </li>
+                  <div className="hamburger-menu-divider" />
+                  <li>
+                    {user ? (
+                      <button
+                        onClick={handleLogout}
+                        className="hamburger-menu-item logout-item"
+                      >
+                        <FaSignOutAlt className="hamburger-item-icon" />
+                        <span>Log Out</span>
+                      </button>
+                    ) : (
+                      <Link
+                        to="/login"
+                        className="hamburger-menu-item login-item"
+                        onClick={() => setMenuOpen(false)}
+                      >
+                        <FaSignInAlt className="hamburger-item-icon" />
+                        <span>Log In</span>
+                      </Link>
+                    )}
+                  </li>
+                </ul>
+              </div>
             </div>
           )}
         </div>
